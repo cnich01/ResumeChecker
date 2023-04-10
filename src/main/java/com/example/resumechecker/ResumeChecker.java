99c532a991a3cc99f5e25b740d884d4b0eed18fd
cnich01 <caitlin_nichols@yahoo.com>
2023-04-10 11:50:41 -0500

    made stage resizable and added in a scroll pane to make the application work with multiple screen sizes/resolutions

--- src/main/java/com/example/resumechecker/ResumeChecker.java
+++ src/main/java/com/example/resumechecker/ResumeChecker.java
@@ -2,6 +2,7 @@ package com.example.resumechecker;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +11,9 @@ public class ResumeChecker extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ResumeChecker.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 1064);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 700);
         stage.setTitle("Resume Checker");
+        stage.setResizable(stage.isResizable());
         stage.setScene(scene);
         stage.show();
     }
